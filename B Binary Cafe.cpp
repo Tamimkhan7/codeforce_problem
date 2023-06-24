ecf46eeb0250937daced0d3c6a5d74672229eea6
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-06-25 02:52:37 +0600

    second commit

--- B Binary Cafe.cpp
+++ B Binary Cafe.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define sq(x) (x) * (x)
+#define PI acos(-1.0)
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define mod 1000000007
+
 int main()
 {
+    faster;
     int t;
     cin >> t;
     while (t--)
@@ -9,7 +23,7 @@ int main()
         long long int n, k;
         cin >> n >> k;
         long long int c = pow(2, k);
-        if (c > n)
+        if (k > log2(n))
             cout << n + 1 << endl;
         else
             cout << c << endl;
