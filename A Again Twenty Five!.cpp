2f28d72c60242c24c206e52e6a917caf22886c61
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-09-05 11:00:30 +0600

    second commit

---
+++ A Again Twenty Five!.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define sq(x) (x) * (x)
+#define PI acos(-1.0)
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+typedef long long int ll;
+typedef unsigned long long int llu;
+#define pb push_back
+#define pop pop_back()
+#define len(a) sizeof(a)
+#define lc (n * 2)
+#define rc ((n * 2) + 1)
+int main()
+{
+    faster;
+    llu n;
+    cin >> n;
+    cout << 25 << endl;
+    return 0;
+}
