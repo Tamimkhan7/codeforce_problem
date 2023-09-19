3079973717ef49e18f9444d1cb8a35aca15b2296
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-09-20 00:30:10 +0600

    second commit

--- C Advantage.cpp
+++ C Advantage.cpp
@@ -1,43 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
-#define faster                    \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-#define sq(x) (x) * (x)
-#define PI acos(-1.0)
-#define all(x) x.begin(), x.end()
-#define nl '\n'
-#define mod 1000000007
 void solve()
 {
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> v;
+    vector<int> b;
     for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    int c = 0, k, m, p, q, r;
+    {
+        int a;
+        cin >> a;
+        v.push_back(a);
+        b.push_back(a);
+    }
+    sort(b.begin(), b.end(), greater<int>());
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            k = arr[i];
-            m = arr[j];
-            if (k < m)
-            {
-                p = k - m;
-                k = m;
-            }
-        }
-        cout << p << " ";
+        if (v[i] == b[0])
+            cout << v[i] - b[1] << " ";
+        else
+            cout << v[i] - b[0] << " ";
     }
     cout << endl;
 }
-
 int main()
 {
     int t;
