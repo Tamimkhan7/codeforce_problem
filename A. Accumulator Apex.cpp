73145970f426a434f20d013c5ab24f8a7119e7a8
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-12-14 14:32:26 +0600

    second commiti

---
+++ A. Accumulator Apex.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    int ans = x;
+    int mx=-1e9;
+    while(y--)
+    {
+        int z;
+        while(cin>>z)
+        {
+            ans += z;
+        }
+        mx = max(mx, ans);
+        ans =x;
+    }
+    cout<<mx<<'\n';
+}
