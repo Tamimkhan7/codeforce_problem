a013394ae274a2db31d6e91371461ff6c4b018fa
Tamim khan <mdtamimkhan842@gmail.com>
2023-12-31 13:06:37 +0600

    second commit

---
+++ A Constructive Problems.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+typedef long long int ll;
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+int32_t main()
+{
+    MTK;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << max(a, b) << '\n';
+    }
+}
