80078af8bdc9d65420f8bcc75ec9788076df885a
Tamim khan <mdtamimkhan842@gmail.com>
2024-03-27 04:14:43 +0600

    updated solving problem

--- DIv 2/B. Array Fix.cpp
+++ DIv 2/B. Array Fix.cpp
@@ -23,19 +23,32 @@ void solve()
     }
     for (int i = 0; i < v.size(); i++)
     {
+        vector<int> tem;
         if (v[i] > 9)
         {
-            int ab = v[v.size() - 1];
-            for (int j = v.size() - 1; j > i + 1; j--)
-                v[j + 1] = v[j];
+            // int ab = v[v.size() - 1];
+            for (int j = 0; j < i; j++)
+                tem.push_back(v[j]);
+            // for (int j = v.size() - 1; j > i + 1; j--)
+            //     v[j + 1] = v[j];
 
             int x = v[i] % 10;
             int y = v[i] / 10;
+            tem.push_back(y);
+            tem.push_back(x);
 
-            v.erase(v.begin() + i);
-            v.insert(v.begin() + i, y);
-            v.insert(v.begin() + i + 1, x);
-            v.push_back(ab);
+            for (int j = i + 1; j < (int)v.size(); j++)
+                tem.push_back(v[j]);
+
+            // v.erase(v.begin() + i);
+            // v.insert(v.begin() + i, y);
+            // v.insert(v.begin() + i + 1, x);
+
+            // v.push_back(ab);
+            v.resize((int)tem.size());
+            for (int j = 0; j < tem.size(); j++)
+                v[j] = tem[j];
+
             if (is_sorted(all(v)))
             {
                 cout << "YES" << '\n';
